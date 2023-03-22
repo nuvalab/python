b237a17c64cc59a87ee4042ec0762d86b6bd3fba
nuvalab <estebanl2786@outlook.com>
2023-03-22 09:22:39 -0500

    New Scripts Added or Modified

--- 010-1-ejercicio-sumar-donaciones.py
+++ 010-1-ejercicio-sumar-donaciones.py
@@ -5,10 +5,10 @@ donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=
 total_donations = 0 #Iniciamos esta variable en cero ya que nos va a servir de variable de acumulación
 
 for i in donations.values(): #Al poner el metodo ".values", le estamos indicando al BUCLE FOR, que recorra los valores del diccionario llamado "donations"
-    total_donations += i # El "+=" se conoce como un operador de asignación y lo que indica es: "total_donations = total_donations + i". Entonces en cada iteración se ira sumandoa  la variable "total_donations" lo que valga "i"
+    total_donations += i # El "+=" se conoce como un operador de asignación y lo que indica es: "total_donations = total_donations + i". Entonces en cada iteración se ira sumando a  la variable "total_donations" lo que valga "i"
 
 
 #Lo anterior puede hacerse muy facil tambien con la funcion "sum()"
-#total_donations = sum(donations.values())
+total_donations = sum(donations.values())
 
 print(total_donations)
---
+++ 011-1-metodo-format.py
@@ -0,0 +1,16 @@
+#Ejemplo 1
+
+# a = "Esteban"
+# b = "Colombiano"
+
+# resultado = "El usuario ha dicho que se llama {} y que es {}".format(a,b)
+
+# print(resultado)
+
+
+#Ejemplo 2
+
+respuesta_1 = input("Indica tu nombre: ")
+respuesta_2 = input("Indica tu nacionalidad: ")
+
+print("El usuario dice que se llama {} y que es {}".format(respuesta_1,respuesta_2) )
